3e13efd95eafac8f96c52915461ee1ff9f982c26
rot <appl.rot13@gmail.com>
2025-02-09 14:01:13 +0900

    Add StringExtensions

---
+++ CSharpCatalog.Test/Utilities/Extensions/StringExtensionsTest.cs
@@ -0,0 +1,46 @@
+﻿
+namespace CSharpCatalog.Test.Utilities.Extensions;
+
+using CSharpCatalog.Utilities.Extensions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class StringExtensionsTest
+{
+    [TestMethod]
+    public void SingleQuotedTest()
+    {
+        "test".SingleQuoted().Is("'test'");
+
+        string.Empty.SingleQuoted().Is(string.Empty);
+        ((string?)null).SingleQuoted().IsNull();
+    }
+
+    [TestMethod]
+    public void DoubleQuotedTest()
+    {
+        "test".DoubleQuoted().Is("\"test\"");
+
+        string.Empty.DoubleQuoted().Is(string.Empty);
+        ((string?)null).DoubleQuoted().IsNull();
+    }
+
+    [TestMethod]
+    public void ParenthesizedTest()
+    {
+        "test".Parenthesized().Is("(test)");
+
+        string.Empty.Parenthesized().Is(string.Empty);
+        ((string?)null).Parenthesized().IsNull();
+    }
+
+    [TestMethod]
+    public void SquareBracketedTest()
+    {
+        "test".SquareBracketed().Is("[test]");
+
+        string.Empty.SquareBracketed().Is(string.Empty);
+        ((string?)null).SquareBracketed().IsNull();
+    }
+}
---
+++ CSharpCatalog/Utilities/Extensions/StringExtensions.cs
@@ -0,0 +1,51 @@
+﻿
+namespace CSharpCatalog.Utilities.Extensions;
+
+using System.Diagnostics.CodeAnalysis;
+
+public static class StringExtensions
+{
+    [return: NotNullIfNotNull(nameof(value))]
+    public static string? SingleQuoted(this string? value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            return value;
+        }
+
+        return $"'{value}'";
+    }
+
+    [return: NotNullIfNotNull(nameof(value))]
+    public static string? DoubleQuoted(this string? value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            return value;
+        }
+
+        return $"\"{value}\"";
+    }
+
+    [return: NotNullIfNotNull(nameof(value))]
+    public static string? Parenthesized(this string? value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            return value;
+        }
+
+        return $"({value})";
+    }
+
+    [return: NotNullIfNotNull(nameof(value))]
+    public static string? SquareBracketed(this string? value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            return value;
+        }
+
+        return $"[{value}]";
+    }
+}
