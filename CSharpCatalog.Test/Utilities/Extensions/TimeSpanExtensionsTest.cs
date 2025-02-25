684caa8c2f5b4aa6eec279032325ac56817bed16
rot <appl.rot13@gmail.com>
2025-02-25 10:50:37 +0900

    Add TimeSpanExtensions

---
+++ CSharpCatalog.Test/Utilities/Extensions/TimeSpanExtensionsTest.cs
@@ -0,0 +1,17 @@
+﻿
+namespace CSharpCatalog.Test.Utilities.Extensions;
+
+using CSharpCatalog.Utilities.Extensions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class TimeSpanExtensionsTest
+{
+    [TestMethod]
+    public void TruncateToSecondsTest()
+    {
+        TimeSpan.FromSeconds(1.000).TruncateToSeconds().Is(TimeSpan.FromSeconds(1));
+        TimeSpan.FromSeconds(1.234).TruncateToSeconds().Is(TimeSpan.FromSeconds(1));
+    }
+}
---
+++ CSharpCatalog/Utilities/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CSharpCatalog.Utilities.Extensions;
+
+public static class TimeSpanExtensions
+{
+    public static TimeSpan TruncateToSeconds(this TimeSpan timeSpan)
+    {
+        return TimeSpan.FromTicks(timeSpan.Ticks / TimeSpan.TicksPerSecond * TimeSpan.TicksPerSecond);
+    }
+}
