cd2b70e50f1016dda36d7751a8653645b89a5276
rot <appl.rot13@gmail.com>
2025-02-27 15:34:54 +0900

    Add ArrayExtensions

---
+++ CSharpCatalog.Test/Utilities/Extensions/ArrayExtensionsTest.cs
@@ -0,0 +1,28 @@
+﻿
+namespace CSharpCatalog.Test.Utilities.Extensions;
+
+using CSharpCatalog.Utilities.Extensions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class ArrayExtensionsTest
+{
+    [TestMethod]
+    public void IsNullOrEmptyTest()
+    {
+        new[] { 1, 2, 3 }.IsNullOrEmpty().IsFalse();
+
+        Array.Empty<int>().IsNullOrEmpty().IsTrue();
+        ((int[]?)null).IsNullOrEmpty().IsTrue();
+    }
+
+    [TestMethod]
+    public void IsAllTrueTest()
+    {
+        new[] { true, false }.IsAllTrue().IsFalse();
+        new[] { true, true  }.IsAllTrue().IsTrue();
+
+        Array.Empty<bool>().IsAllTrue().IsTrue();
+    }
+}
