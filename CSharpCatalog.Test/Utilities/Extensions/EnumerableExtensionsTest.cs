d9c1f700b7a276b533940daaf50e82e5b31a171c
rot <appl.rot13@gmail.com>
2025-03-02 17:53:05 +0900

    Add EnumerableExtensions

---
+++ CSharpCatalog.Test/Utilities/Extensions/EnumerableExtensionsTest.cs
@@ -0,0 +1,22 @@
+﻿
+namespace CSharpCatalog.Test.Utilities.Extensions;
+
+using CSharpCatalog.Utilities.Extensions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class EnumerableExtensionsTest
+{
+    [TestMethod]
+    public void ExcludeNullStructTest()
+    {
+        new int?[] { 1, null, 2, null, 3 }.ExcludeNull().ToArray().IsStructuralEqual(new[] { 1, 2, 3 });
+    }
+
+    [TestMethod]
+    public void ExcludeNullClassTest()
+    {
+        new[] { "1", null, "2", null, "3" }.ExcludeNull().ToArray().IsStructuralEqual(new[] { "1", "2", "3" });
+    }
+}
