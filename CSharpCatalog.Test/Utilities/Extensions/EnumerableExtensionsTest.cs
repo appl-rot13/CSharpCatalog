81222820c1df228ba7e2df508478ad899d9517d1
rot <appl.rot13@gmail.com>
2025-03-04 10:46:20 +0900

    Migrate from ChainingAssertion to Shouldly

--- CSharpCatalog.Test/Utilities/Extensions/EnumerableExtensionsTest.cs
+++ CSharpCatalog.Test/Utilities/Extensions/EnumerableExtensionsTest.cs
@@ -5,18 +5,20 @@ using CSharpCatalog.Utilities.Extensions;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using Shouldly;
+
 [TestClass]
 public class EnumerableExtensionsTest
 {
     [TestMethod]
     public void ExcludeNullStructTest()
     {
-        new int?[] { 1, null, 2, null, 3 }.ExcludeNull().ToArray().IsStructuralEqual(new[] { 1, 2, 3 });
+        new int?[] { 1, null, 2, null, 3 }.ExcludeNull().ShouldBe([ 1, 2, 3 ]);
     }
 
     [TestMethod]
     public void ExcludeNullClassTest()
     {
-        new[] { "1", null, "2", null, "3" }.ExcludeNull().ToArray().IsStructuralEqual(new[] { "1", "2", "3" });
+        new[] { "1", null, "2", null, "3" }.ExcludeNull().ShouldBe([ "1", "2", "3" ]);
     }
 }
